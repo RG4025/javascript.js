539a6d9a8e6eed2d89b04e4a3744e1806862f7b2
Rushi <148181969+RG4025@users.noreply.github.com>
2023-12-15 14:26:32 +0000

    New Commit

---
+++ javascript_exercise/array_slice_slice/javascript/script.js
@@ -0,0 +1,61 @@
+
+        /*
+        // using the slice method we can able to print the value between the two numbers... but the origianl array are ramains same
+        const arr_one =[1,2,3,4,5];
+        let first = arr_one.slice(1,3);
+        console.log(first);
+        console.log(arr_one);
+
+
+        // using the splice method we can able to print the value from one value to the last given index value .. but the splice method changes the origial length of the array
+        let second = arr_one.splice(1,3);
+        console.log(second);
+        console.log(arr_one);
+
+
+        let marks_one = 100;
+        let marks_two = 90;
+        let marks_three = 80;
+
+        const combination_arr = Array.of(marks_one,marks_two,marks_three);
+        console.log(combination_arr);
+
+
+        let arry_first = [10,10,"Rushi",true];
+        let second_first = [9,32,45,"rg"];
+
+        const combo = [...arry_first, ...second_first];
+        console.log(combo);
+*/
+
+
+function first(num1, num2){
+
+    return (num1 + num2 );
+
+}
+
+let emran = first(1,2)
+console.log(emran);
+
+var first = 10;
+var first = 40;
+
+
+let second;
+
+second = 40;
+
+// const third ;
+
+{
+    let a = 10;
+    console.log(a);
+}
+
+console.log(a);
+
+const obj = {
+    name : "emran",
+    id : 98,
+}
