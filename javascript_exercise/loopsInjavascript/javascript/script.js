ff43790ab5e4d5c5216f60a4b21bdbe5920f3ac5
Rushi <148181969+RG4025@users.noreply.github.com>
2024-01-03 11:18:01 +0000

    New Commit

---
+++ javascript_exercise/loopsInjavascript/javascript/script.js
@@ -0,0 +1,108 @@
+ /* let sum = 0;
+
+        let n = prompt("Enter your number :");
+        n = Number.parseInt(n);
+        for(let i=0; i<n; i++){
+        sum+= i+1;
+            }
+        alert("Sum of first" + n + "natural numbers is :" + sum);
+        */
+
+
+        // let n = prompt("write the number of which you wanna see the multiplication table of")
+        // for (let i = 1; i <= 10; i++) {
+
+        //   console.log( (n * i))
+        // }
+
+
+
+
+
+        /*
+        let obj = {
+            first : "rushi",
+            second : "vicky",
+            third : "rg"
+        }
+        
+        for(let a in obj){
+            console.log("name of the " + a + " member is :" + obj[a]);
+        
+        }
+        
+        for(let b of "rushi"){
+            console.log(b);
+        
+        }
+        
+        */
+
+
+
+        /*
+        let num = prompt("Enter the number :");
+        num = Number.parseInt(num);
+        let i = 0;
+        
+        do{
+            console.log(i+1);
+            i++;
+        }
+        while(i<num)
+        */
+
+
+
+
+        /*
+         for(let i=1; i<=10 ; i++){
+            document.write(i + " " + ",");
+        
+        }
+        */
+
+
+
+        /*
+        let i = 1;
+        
+        while(i<=20){
+            document.write(i + " " + ",");
+            i++;
+        
+        }
+        */
+
+
+
+        /*
+        let i = 1;
+        do{
+            document.write(i + " " + "<br>");
+            i++;
+        
+        }
+        while(i<=30);
+        */
+
+
+
+        /*
+        let var1 ;
+        
+        const rg = {
+            name :"Rushi",
+            last_name : "Gurav",
+            Mob_number : 7927862783
+        }
+        rg["address"] = ["Pune"];
+        
+        for(let a in rg){
+            document.write("The Typed information ig given below : " + " "+ a +" "+ rg[a] + " <br>");
+        }
+        
+        for(let b of rg["name"]){
+            document.write(b);
+        }
+        */
